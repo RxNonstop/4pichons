7c6f9c4138e60efc1bf2820b59815a3f0c4e2716
Eduardo Almanza <ealmanza@russ.com.co>
2024-04-28 22:37:15 -0500

    fix image

--- src/sections/inicio.js
+++ src/sections/inicio.js
@@ -1,20 +1,39 @@
 import React from "react";
 import MenuLateral from "../components/MenuLateral";
+import BannerImg from "../assets/images/img_inicio.png";
 
 function Inicio() {
-    return (
-        <section className='inicio' id="inicio">
-          <div className='text_inicio'>
-            <h3>A TU ALCANCE</h3>
-            <h1>ESTÁ EL METAVERSO</h1>
-            <p>"¡Sumérgete en la realidad virtual y haz que tus emociones cobren vida con nosotros!"</p>
-            <button>DESCUBRELO</button>
-          </div>
-          <div className="contenedor-menu">
-            <MenuLateral/>
-          </div>
-        </section>
-    );
-  }
-  
-  export default Inicio;
+  return (
+    <section className="inicio" id="inicio">
+      <img
+        style={{
+          position: "absolute",
+          objectFit: "cover",
+          right: "0",
+          bottom: "-175px",
+          maskImage: "linear-gradient(black 75%, transparent, transparent)",
+          width: "68vw",
+          zIndex: "-1",
+        }}
+        width="100%"
+        height="100%"
+        alt="banner"
+        src={BannerImg}
+      />
+      <div className="text_inicio">
+        <h3>A TU ALCANCE</h3>
+        <h1>ESTÁ EL METAVERSO</h1>
+        <p>
+          "¡Sumérgete en la realidad virtual y haz que tus emociones cobren vida
+          con nosotros!"
+        </p>
+        <button>DESCUBRELO</button>
+      </div>
+      <div className="contenedor-menu">
+        <MenuLateral />
+      </div>
+    </section>
+  );
+}
+
+export default Inicio;
