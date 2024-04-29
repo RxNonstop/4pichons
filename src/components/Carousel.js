f9724187c564a95e2daf17d4a5eba2906bae0961
RxNonstop <aljotana2001@gmail.com>
2024-04-28 22:47:24 -0500

    carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,30 +1,20 @@
 import Carousel from 'react-bootstrap/Carousel';
+import video1 from '../assets/videos/video1.mp4'
 
 function CarouselVideo() {
   return (
     <Carousel>
-      <Carousel.Item >
-        
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item >
-        
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item >
-        
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
+      <Carousel.Item>
+        <div className='video-container'>
+          <video src={video1} autoPlay muted className='video_content'>
+          </video>
+          <div className='carousel-caption'>
+            <div className='text'>
+              <h1>¿que ofrecemos?</h1>
+              <p>Nos enorgullece ofrecer una amplia gama de servicios  diseñados para llevar tus eventos corporativos al siguiente nivel. Nuestro objetivo es brindar experiencias únicas y memorables que impulsen el éxito de su empresa.</p>
+            </div>
+          </div>
+        </div>
       </Carousel.Item>
     </Carousel>
   );
