f94b32631d41a355c2e15e0782ab03c5b125592b
RxNonstop <aljotana2001@gmail.com>
2024-04-30 00:40:02 -0500

    agregado video 2

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -17,6 +17,18 @@ function CarouselVideo() {
           </video>
           <div className='carousel-caption'>
             <div className='text'>
+              <h1>¿quienes somos?</h1>
+              <p>Una empresa líder en la creación y ejecución de eventos corporativos innovadores desde el punto de diseño hasta la coordinación.</p>
+            </div>
+          </div>
+        </div>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div className='video-container'>
+          <video src={video1} autoPlay muted loop className='video_content'>
+          </video>
+          <div className='carousel-caption'>
+            <div className='text'>
               <h1>¿que ofrecemos?</h1>
               <p>Nos enorgullece ofrecer una amplia gama de servicios  diseñados para llevar tus eventos corporativos al siguiente nivel. Nuestro objetivo es brindar experiencias únicas y memorables que impulsen el éxito de su empresa.</p>
             </div>
