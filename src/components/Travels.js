01520bbc91b80300677cb982ebafe491a3218760
RxNonstop <aljotana2001@gmail.com>
2024-05-29 23:25:25 -0500

    imagenes galeria

--- src/components/Travels.js
+++ src/components/Travels.js
@@ -8,14 +8,14 @@ function Travels(){
 
     const images = [
         { id: 1, title: 'VANCOUVER', src: require('../assets/images/VANCOUVER.png'), text: 'Genera colaboración entre equipos de trabajo con TEAM BUILDING OPS, un ideal escenario de acción donde por medio del uso de REALIDAD MIXTA los participantes desarrollan capacidades de liderazgo, comunicación y resolución de conflictos bajo presión en un entorno dinámico de juego y estrategia.' },
-        { id: 2, title: 'ALASKA', src: require('../assets/images/VANCOUVER.png'), text: 'Genera colaboración entre equipos de trabajo con TEAM BUILDING OPS, un ideal escenario de acción donde por medio del uso de REALIDAD MIXTA los participantes desarrollan capacidades de liderazgo, comunicación y resolución de conflictos bajo presión en un entorno dinámico de juego y estrategia.' },
-        { id: 3, title: 'VANCOUVER', src: require('../assets/images/VANCOUVER.png'), text: 'Genera colaboración entre equipos de trabajo con TEAM BUILDING OPS, un ideal escenario de acción donde por medio del uso de REALIDAD MIXTA los participantes desarrollan capacidades de liderazgo, comunicación y resolución de conflictos bajo presión en un entorno dinámico de juego y estrategia.' },
-        { id: 4, title: 'TORONTO', src: require('../assets/images/VANCOUVER.png'), text: 'Genera colaboración entre equipos de trabajo con TEAM BUILDING OPS, un ideal escenario de acción donde por medio del uso de REALIDAD MIXTA los participantes desarrollan capacidades de liderazgo, comunicación y resolución de conflictos bajo presión en un entorno dinámico de juego y estrategia.' },
-        { id: 5, title: 'DISNEY', src: require('../assets/images/VANCOUVER.png'), text: 'Genera colaboración entre equipos de trabajo con TEAM BUILDING OPS, un ideal escenario de acción donde por medio del uso de REALIDAD MIXTA los participantes desarrollan capacidades de liderazgo, comunicación y resolución de conflictos bajo presión en un entorno dinámico de juego y estrategia.' },
-        { id: 6, title: 'SIMULADOR DE VUELO', src: require('../assets/images/VANCOUVER.png'), text: 'Genera colaboración entre equipos de trabajo con TEAM BUILDING OPS, un ideal escenario de acción donde por medio del uso de REALIDAD MIXTA los participantes desarrollan capacidades de liderazgo, comunicación y resolución de conflictos bajo presión en un entorno dinámico de juego y estrategia.' },
-        { id: 7, title: 'DISNEY', src: require('../assets/images/VANCOUVER.png'), text: 'Genera colaboración entre equipos de trabajo con TEAM BUILDING OPS, un ideal escenario de acción donde por medio del uso de REALIDAD MIXTA los participantes desarrollan capacidades de liderazgo, comunicación y resolución de conflictos bajo presión en un entorno dinámico de juego y estrategia.' },
-        { id: 8, title: 'TORONTO', src: require('../assets/images/VANCOUVER.png'), text: 'Genera colaboración entre equipos de trabajo con TEAM BUILDING OPS, un ideal escenario de acción donde por medio del uso de REALIDAD MIXTA los participantes desarrollan capacidades de liderazgo, comunicación y resolución de conflictos bajo presión en un entorno dinámico de juego y estrategia.' },
-        { id: 9, title: 'ALASKA', src: require('../assets/images/VANCOUVER.png'), text: 'Genera colaboración entre equipos de trabajo con TEAM BUILDING OPS, un ideal escenario de acción donde por medio del uso de REALIDAD MIXTA los participantes desarrollan capacidades de liderazgo, comunicación y resolución de conflictos bajo presión en un entorno dinámico de juego y estrategia.' },
+        { id: 2, title: 'ALASKA', src: require('../assets/images/ALASKA.png'), text: 'Genera colaboración entre equipos de trabajo con TEAM BUILDING OPS, un ideal escenario de acción donde por medio del uso de REALIDAD MIXTA los participantes desarrollan capacidades de liderazgo, comunicación y resolución de conflictos bajo presión en un entorno dinámico de juego y estrategia.' },
+        { id: 3, title: 'VANCOUVER', src: require('../assets/images/VANCOUVER2.png'), text: 'Genera colaboración entre equipos de trabajo con TEAM BUILDING OPS, un ideal escenario de acción donde por medio del uso de REALIDAD MIXTA los participantes desarrollan capacidades de liderazgo, comunicación y resolución de conflictos bajo presión en un entorno dinámico de juego y estrategia.' },
+        { id: 4, title: 'TORONTO', src: require('../assets/images/TORONTO.png'), text: 'Genera colaboración entre equipos de trabajo con TEAM BUILDING OPS, un ideal escenario de acción donde por medio del uso de REALIDAD MIXTA los participantes desarrollan capacidades de liderazgo, comunicación y resolución de conflictos bajo presión en un entorno dinámico de juego y estrategia.' },
+        { id: 5, title: 'DISNEY', src: require('../assets/images/DISNEY2.png'), text: 'Genera colaboración entre equipos de trabajo con TEAM BUILDING OPS, un ideal escenario de acción donde por medio del uso de REALIDAD MIXTA los participantes desarrollan capacidades de liderazgo, comunicación y resolución de conflictos bajo presión en un entorno dinámico de juego y estrategia.' },
+        { id: 6, title: 'VICTORIA', src: require('../assets/images/VICTORIA.png'), text: 'Genera colaboración entre equipos de trabajo con TEAM BUILDING OPS, un ideal escenario de acción donde por medio del uso de REALIDAD MIXTA los participantes desarrollan capacidades de liderazgo, comunicación y resolución de conflictos bajo presión en un entorno dinámico de juego y estrategia.' },
+        { id: 7, title: 'DISNEY', src: require('../assets/images/DISNEY.png'), text: 'Genera colaboración entre equipos de trabajo con TEAM BUILDING OPS, un ideal escenario de acción donde por medio del uso de REALIDAD MIXTA los participantes desarrollan capacidades de liderazgo, comunicación y resolución de conflictos bajo presión en un entorno dinámico de juego y estrategia.' },
+        { id: 8, title: 'TORONTO', src: require('../assets/images/TORONTO2.png'), text: 'Genera colaboración entre equipos de trabajo con TEAM BUILDING OPS, un ideal escenario de acción donde por medio del uso de REALIDAD MIXTA los participantes desarrollan capacidades de liderazgo, comunicación y resolución de conflictos bajo presión en un entorno dinámico de juego y estrategia.' },
+        { id: 9, title: 'ALASKA', src: require('../assets/images/ALASKA2.png'), text: 'Genera colaboración entre equipos de trabajo con TEAM BUILDING OPS, un ideal escenario de acción donde por medio del uso de REALIDAD MIXTA los participantes desarrollan capacidades de liderazgo, comunicación y resolución de conflictos bajo presión en un entorno dinámico de juego y estrategia.' },
     ];
 
     const handleClick = (imgId) => {
@@ -34,11 +34,11 @@ function Travels(){
     return(
         <div className="container_photos">
             {isExpanded === false ? (
-                <div className="video_gallery">
+                <div className="image_gallery">
                 {images.map((image) => (
-                    <div key={image.id} onClick={() => {handleModal(); handleClick(image.id);}}>
+                    <div key={image.id} onClick={() => {handleModal(); handleClick(image.id);}} className="space_img">
                         <div className="thumbnail_container">
-                        <div className="video_container">
+                        <div className="image_container">
                             <img src={image.src} alt=""></img>
                         </div>
                         <h3>{image.title}</h3>
