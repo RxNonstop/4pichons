9aa7cc987ad33e6260c7b940785ee3bbb6dfd968
RxNonstop <aljotana2001@gmail.com>
2024-06-10 15:12:12 -0500

    galeria de imagenes

--- src/components/GalleryImages.js
+++ src/components/GalleryImages.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const GalleryImages = () => {
+
+    const [selectedPage, setSelectedPage] = useState(0)
 
     const images = [
         require('../assets/images/gallery_image4.png'),
@@ -19,27 +21,53 @@ const GalleryImages = () => {
         require('../assets/images/gallery_image1.png'),
     ];
 
+    const handleSelectedPage = (op) =>{
+        if(op===0){
+            setSelectedPage(selectedPage-1)
+        }
+        else if(op===1){
+            setSelectedPage(selectedPage+1)
+        }
+    }
+
     return (
-        <div className='gallery_images'>
-            <div className='gallery_wrapper'>
-                <div className='group_wrapper'>
-                    <img src={images[4]} alt='' className='gallery-item'></img>
-                    <img src={images[7]} alt='' className='gallery-item'></img>
-                    <img src={images[13]} alt='' className='gallery-item'></img>
+        <div className='gallery_mix'>
+            <div className='gallery_interactions'>
+                <button onClick={()=>handleSelectedPage(0)}>Anterior</button>
+            </div>
+            {selectedPage === 0 && (
+                <div className='gallery_images1'>
+                    <div className='gallery_wrapper'>
+                        <div className='group_wrapper'>
+                            <img src={images[4]} alt='' className='gallery-item'></img>
+                            <img src={images[7]} alt='' className='gallery-item'></img>
+                            <img src={images[13]} alt='' className='gallery-item'></img>
+                        </div>
+                        <div className='gallery_title'>GALERIA</div>
+                        <div className='group_wrapper'>
+                            <img src={images[1]} alt='' className='gallery-item'></img>
+                            <img src={images[9]} alt='' className='gallery-item'></img>
+                            <img src={images[0]} alt='' className='gallery-item'></img>
+                        </div>
+                    </div>
                 </div>
-                <img src={images[3]} alt='' className='gallery-item'></img>
-                <div className='group_wrapper'>
-                    <img src={images[1]} alt='' className='gallery-item'></img>
-                    <img src={images[9]} alt='' className='gallery-item'></img>
-                    <div className='gallery_title'>GALERIA</div>
+            )}
+            {selectedPage === 1 && (
+                <div className='gallery_images2'>
+                    <div className='gallery_wrapper2'>
+                        <img src={images[12]} alt='' className='gallery-item2'></img>
+                        <img src={images[3]} alt='' className='gallery-item2'></img>
+                        <img src={images[5]} alt='' className='gallery-item2'></img>
+                        <img src={images[6]} alt='' className='gallery-item2'></img>
+                        <img src={images[8]} alt='' className='gallery-item2'></img>
+                        <img src={images[10]} alt='' className='gallery-item2'></img>
+                        <img src={images[11]} alt='' className='gallery-item2'></img>
+                        <img src={images[2]} alt='' className='gallery-item2'></img>
+                    </div>
                 </div>
-                <img src={images[6]} alt='' className='gallery-item'></img>
-                <img src={images[12]} alt='' className='gallery-item'></img>
-                <img src={images[8]} alt='' className='gallery-item'></img>
-                <img src={images[11]} alt='' className='gallery-item'></img>
-                <img src={images[5]} alt='' className='gallery-item'></img>
-                <img src={images[10]} alt='' className='gallery-item'></img>
-                <img src={images[2]} alt='' className='gallery-item'></img>
+            )}
+            <div className='gallery_interactions'>
+                <button onClick={()=>handleSelectedPage(1)}>Siguiente</button>
             </div>
         </div>
     );
